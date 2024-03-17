baf36d79a1de8d0f612ecb038d6ce92f6fb2bc90
v <mondalbdyut@gmail.com>
2024-03-17 09:43:38 +0530

    websocket 002

--- src/app/user/chat-box/chat-box.component.ts
+++ src/app/user/chat-box/chat-box.component.ts
@@ -15,10 +15,11 @@ export class ChatBoxComponent implements OnInit {
   ) { }
 // private echo: Echo
 envdata:any = environment;
+
 wsdata:any='';
   ngOnInit(): void {
     this.websocket();
-    this.getChatUserlist();
+    // this.getChatUserlist();
   }
 
   websocket(){
@@ -26,33 +27,47 @@ wsdata:any='';
       broadcaster: 'pusher',
       key: 'ABCDEFGH',
       cluster: 'mt1',
-      wsHost: window.location.hostname,
+      // wsHost: window.location.hostname, // this also work
+      wsHost: `localhost`,// this also work
       wsPort: 6001,
       forceTLS: false,
       disableStats: true,
-      enabledTransports:['ws']
+      enabledTransports:['ws'],
+      authEndpoint:`${this.envdata.apiUrl}/myblog/access/broadcasting/auth`,
+      auth:{
+        headers:{
+          Authorization:`Bearer ${this.APIservice.loggedinuserdata().token}`,
+          // 'Content-Type':'multipart/form-data'
+        }
+      }
     });
-      // WS.join('public-channel')
-      //   .here((user:any)=>{
-      //       console.log("user",user);
-      //   }) 
-      //   .joining((user:any)=>{
-      //       console.log("joining",user);
-      //   }) 
-      //   .leaving((user:any)=>{
-      //       console.log("leaving",user);
-      //   })
-      //   .listen('Publicchannel', (event:any) => {
-      //       console.log("data",event);
-      //  })
-      // https://github.com/tald7344/laravel-ngrx-ecommerce/tree/websocket
-    WS.channel('public-channel')
+    
+      WS.join('track-public-channel')
+        .here((user:any)=>{
+            console.log("user",user);
+        }) 
+        .joining((user:any)=>{
+            console.log("joining",user);
+        }) 
+        .leaving((user:any)=>{
+            console.log("leaving",user);
+        })
         .listen('Publicchannel', (event:any) => {
             console.log("data",event);
-            this.wsdata=event;
-        }).error((e:any)=>{
-          console.log('error',e);
-        });
+        }).error((error:any)=>{
+          console.log("error",error);
+       });
+      // https://github.com/tald7344/laravel-ngrx-ecommerce/tree/websocket
+    // WS.channel('public-channel')
+    //     .listen('Publicchannel', (event:any) => {
+    //         console.log("data",event);
+    //         this.wsdata=event;
+    //     }).error((e:any)=>{
+    //       console.log('error',e);
+    //     });
+       setTimeout(()=>{
+ console.log(WS.socketId());
+       },2000)
         // this.echo.connector.socket.on('close', (close:any) => {
         // console.log('close',close);
         // })
